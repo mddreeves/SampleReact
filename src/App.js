1f021ef060d499cb0174fedba51e4c4ea8ed9bc7
Mike Reeves <mddreeves@hotmail.com>
2021-11-05 20:27:06 -0400

    more changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo2} className="App-logo" alt="logo2" />
+        <img src={logo2} />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Document Respository Search powered by AWS Kendra
